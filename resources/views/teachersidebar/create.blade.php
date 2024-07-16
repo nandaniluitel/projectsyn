45bb0632ea9fd4e724b9351e7541691d7300f551
Ansaaad <anushadhakal9@gmail.com>
2024-07-16 10:29:56 +0545

    project Synergy

--- resources/views/teachersidebar/create.blade.php
+++ resources/views/teachersidebar/create.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4 ">
     <!-- Brand Logo -->
-    <a href="/adminlte/index3.html" class="brand-link">
+    <a href="" class="brand-link">
       <img src="/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Project Synergy</span>
     </a>
